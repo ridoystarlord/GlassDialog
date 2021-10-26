3aa5c110e460a233afb0c537a83685fc74e2b5e1
Mehedi Hasan Ridoy <ridoy.starlord@gmail.com>
2021-10-26 19:03:02 +0600

    Some Fixes

--- app/src/main/java/com/ridoy/glassdialog/MainActivity.java
+++ app/src/main/java/com/ridoy/glassdialog/MainActivity.java
@@ -18,10 +18,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         successBtn=findViewById(R.id.successBtn);
-        errorBtn=findViewById(R.id.errorBtn);
-        infoBtn=findViewById(R.id.infoBtn);
-        warningBtn=findViewById(R.id.warningBtn);
-        customBtn=findViewById(R.id.customBtn);
 
         successBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,77 +36,5 @@ public class MainActivity extends AppCompatActivity {
                 glassdialog.show();
             }
         });
-        errorBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                glassdialog=new Glassdialog(MainActivity.this,Glassdialog.ERROR_TYPE);
-                glassdialog.setTitle("Error Glass Dialog");
-                glassdialog.setConfirmButtonText("Ok");
-                glassdialog.setCancelButtonText("Cancel");
-                glassdialog.setMessage("Hi Bro, I am new born glass dialog.\nDid you like me?");
-                glassdialog.setConfirmClickListener(new Glassdialog.Glasslisner() {
-                    @Override
-                    public void onClick(Glassdialog dialog) {
-                        dialog.dismiss();
-                        Toast.makeText(getApplicationContext(), "Successfully Created", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                glassdialog.show();
-            }
-        });
-        infoBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                glassdialog=new Glassdialog(MainActivity.this,Glassdialog.INFO_TYPE);
-                glassdialog.setTitle("Info Glass Dialog");
-                glassdialog.setConfirmButtonText("Ok");
-                glassdialog.setMessage("Hi Bro, I am new born glass dialog.\nDid you like me?");
-                glassdialog.setConfirmClickListener(new Glassdialog.Glasslisner() {
-                    @Override
-                    public void onClick(Glassdialog dialog) {
-                        dialog.dismiss();
-                        Toast.makeText(getApplicationContext(), "Successfully Created", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                glassdialog.show();
-            }
-        });
-        warningBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                glassdialog=new Glassdialog(MainActivity.this,Glassdialog.WARNING_TYPE);
-                glassdialog.setTitle("Warning Glass Dialog");
-                glassdialog.setConfirmButtonText("Ok");
-                glassdialog.setCancelButtonText("Cancel");
-                glassdialog.setMessage("Hi Bro, I am new born glass dialog.\nDid you like me?");
-                glassdialog.setConfirmClickListener(new Glassdialog.Glasslisner() {
-                    @Override
-                    public void onClick(Glassdialog dialog) {
-                        dialog.dismiss();
-                        Toast.makeText(getApplicationContext(), "Successfully Created", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                glassdialog.show();
-            }
-        });
-        customBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                glassdialog=new Glassdialog(MainActivity.this,Glassdialog.CUSTOM_TYPE,"team.json");
-                glassdialog.setTitle("Custom Glass Dialog");
-                glassdialog.setConfirmButtonText("Ok");
-                glassdialog.setMessage("Hi Bro, I am new born glass dialog.\nDid you like me?");
-                glassdialog.setConfirmClickListener(new Glassdialog.Glasslisner() {
-                    @Override
-                    public void onClick(Glassdialog dialog) {
-                        dialog.dismiss();
-                        Toast.makeText(getApplicationContext(), "Successfully Created", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                glassdialog.show();
-            }
-        });
-
-
     }
 }
